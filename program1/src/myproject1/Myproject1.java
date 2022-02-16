206512ee796106cb4ca258742504ab542f58bb4b
Amit kumar kushwaha <ajeet.k201515@gmail.com>
2022-02-16 22:32:09 +0530

    Project 1 check-in

---
+++ program1/src/myproject1/Myproject1.java
@@ -0,0 +1,16 @@
+package myproject1;
+
+public class Myproject1 
+{
+
+	public static void main(String[] args)
+	{
+		int i=3;
+		for(i=3;i<=7;i++)
+		{
+			System.out.println("Hello");
+		}
+
+	}
+
+}
